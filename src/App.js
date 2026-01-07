e4348492deaff8493bd35db9297d6fd04b738c8e
luisfelip2005 <gomesluisfelipe09@gmail.com>
2026-01-06 21:56:56 -0300

    building html body and styling it

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Main from './pages/Main/Main';
+
+function App() {
+  return (
+    <Main />
+  );
+}
+
+export default App;
---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import './main.css';
+
+export default function Main() {
+  return (
+    <section className='main-section'>
+      <div className="container">
+        <div className="calendar-container">
+          <div className="calendar-header">
+            <div className='header1'>
+              <div>
+                <span>Timetable</span>
+                <span>Today</span>
+                <span>Done</span>
+              </div>
+              <div>
+                <span>Filter</span>
+                <span>Sort</span>
+                <span>Search</span>
+              </div>
+            </div>
+            <div className='header2'>
+              <span>January 2026</span>
+              <span>Today</span>
+            </div>
+            <div className="calendar">
+              <div className="div-date">
+                <span>Sun</span>
+                <div className="div-content">
+                  <span>4</span>
+                  <div className='div-task'>
+                    <div>Running</div>
+                    <span>Any Time</span>
+                  </div>
+                  <div className='div-task'>
+                    <div>Running</div>
+                    <span>Any Time</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="div-date">
+                <span>Sun</span>
+                <div className="div-content">
+                  <span>4</span>
+                  <div className='div-task'>
+                    <div>Running</div>
+                    <span>Any Time</span>
+                  </div>
+                </div>
+              </div>
+              <div className="div-date">
+                <span>Sun</span>
+                <div className="div-content">
+                  <span>4</span>
+                  <div className='div-task'>
+                    <div>Running</div>
+                    <span>Any Time</span>
+                  </div>
+                </div>
+              </div><div className="div-date">
+                <span>Sun</span>
+                <div className="div-content">
+                  <span>4</span>
+                  <div className='div-task'>
+                    <div>Running</div>
+                    <span>Any Time</span>
+                  </div>
+                </div>
+              </div><div className="div-date">
+                <span>Sun</span>
+                <div className="div-content">
+                  <span>4</span>
+                  <div className='div-task'>
+                    <div>Running</div>
+                    <span>Any Time</span>
+                  </div>
+                </div>
+              </div><div className="div-date">
+                <span>Sun</span>
+                <div className="div-content">
+                  <span>4</span>
+                  <div className='div-task'>
+                    <div>Running</div>
+                    <span>Any Time</span>
+                  </div>
+                </div>
+              </div><div className="div-date">
+                <span>Sun</span>
+                <div className="div-content">
+                  <span>4</span>
+                  <div className='div-task'>
+                    <div>Running</div>
+                    <span>Any Time</span>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
