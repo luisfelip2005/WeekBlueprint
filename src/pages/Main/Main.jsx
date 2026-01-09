d530eb1de89349005a6ae15eec3eaaa00f501026
luisfelip2005 <gomesluisfelipe09@gmail.com>
2026-01-08 21:25:24 -0300

    creating modal and starting its html body

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,9 +1,36 @@
 import React, {useEffect, useState} from 'react';
+import Modal from 'react-modal';
 import './main.css';
 
 export default function Main() {
   const [week, setWeek] = useState([])
   const [indexHovered, setIndexHovered] = useState(null)
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   useEffect(() => {
     const today = new Date()
@@ -59,7 +86,7 @@ export default function Main() {
                     <span className='week-day'>{date.dayOfWeek}</span>
                     <div className="div-content">
                       <span className='div-content-header'>
-                        {indexHovered == index ? <button>+</button> : <button className='fake-button'>+</button>}
+                        {indexHovered == index ? <button onClick={openModal}>+</button> : <button className='fake-button'>+</button>}
                         <span>{date.day}</span>
                       </span>
                       <div className='div-task'>
@@ -78,6 +105,48 @@ export default function Main() {
           </div>
         </div>
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        className="Modal"
+        overlayClassName="Overlay"
+        contentLabel="Example Modal"
+      >
+        <section>
+          <div className="container">
+            <form className='form-add-task'>
+              <input className='input-title' value={"New Task"} />
+              <span>
+                <label htmlFor="input-date">Date</label>
+                <div>
+                  <input type="date" />
+                </div>
+              </span>
+              <span>
+                <label htmlFor="task-status">Completed</label>
+                <div>
+                  <input type="checkbox" />
+                </div>
+              </span>
+              <span>
+                <label htmlFor="last-edited">Last edited time</label>
+                <div>
+                  <p>January 8, 2026 7:56 PM</p>
+                </div>
+              </span>
+            </form>
+            <div className="task-description">
+
+            </div>
+            <div className="select-task">
+
+            </div>
+          </div>
+        </section>
+      </Modal>
     </section>
   )
 }
